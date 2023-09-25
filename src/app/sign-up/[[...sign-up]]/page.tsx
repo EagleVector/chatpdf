5207165329d42513b3f2814cc9fd4e5d476be7c9
EagleVector <kumarsubhambili3@gmail.com>
2023-09-25 11:23:45 +0530

    clerk sign-up component

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+ 
+export default function Page() {
+  return (
+    <div className="absolute top-1/3 left-1/3 -trabslate-x-1/2 -translate-y-1/2">
+      <SignUp />
+    </div>
+  )
+}
