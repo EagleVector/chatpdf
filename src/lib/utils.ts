2fd7dd7aad9fed3b8d3a05a16da25d59fdb23f4c
EagleVector <kumarsubhambili3@gmail.com>
2023-09-29 11:36:35 +0530

    Webhook Setup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// export function convertToAscii(inputString: string) {
-//   // remove non ascii characters
-//   const asciiString = inputString.replace(/[^\x00-\x7F]+/g, "");
-//   return asciiString;
-// }
+export function convertToAscii(inputString: string) {
+  // remove non ascii characters
+  const asciiString = inputString.replace(/[^\x00-\x7F]+/g, "");
+  return asciiString;
+}
