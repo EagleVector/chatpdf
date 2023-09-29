9fffef1ec5312fbaef4e010d8ac89fe466eea1f6
EagleVector <kumarsubhambili3@gmail.com>
2023-09-29 11:32:05 +0530

    page render

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,8 +1,10 @@
 // /chat/chatId
+import ChatComponent from '@/components/ChatComponent';
 import ChatSidebar from '@/components/ChatSidebar';
 import PDFViewer from '@/components/PDFViewer';
 import { db } from '@/lib/db';
 import { chats } from '@/lib/db/schema';
+import { checkSubscription } from '@/lib/subscription';
 import { auth } from '@clerk/nextjs';
 import { eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
@@ -28,14 +30,15 @@ const ChatPage = async ({ params: { chatId } }: Props) => {
     return redirect("/");
   }
 
-  const currentChat = _chats.find(chat => chat.id === parseInt(chatId))
+  const currentChat = _chats.find(chat => chat.id === parseInt(chatId));
+  const isPro = await checkSubscription();
 
   return (
     <div className='flex max-h-screen overflow-scroll'>
       <div className='flex w-full max-h-screen overflow-scroll'>
         {/* chat sidebar */}
         <div className='flex-[2] max-w-xs'>
-          <ChatSidebar chats={_chats} chatId={parseInt(chatId)} />
+          <ChatSidebar chats={_chats} chatId={parseInt(chatId)} isPro={isPro} />
         </div>
         {/* pdf viewer */}
         <div className='flex-[4] max-h-screen p-4 overflow-scroll'>
@@ -43,7 +46,7 @@ const ChatPage = async ({ params: { chatId } }: Props) => {
         </div>
         {/* chat component */}
         <div className='flex-[2] border-1-4 border-1-slate-200'>
-          {/* <ChatComponent /> */}
+          <ChatComponent chatId={parseInt(chatId)} />
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { auth, UserButton } from "@clerk/nextjs";
-import Link from "next/link";
-import { LogInIcon } from 'lucide-react'
+import { ArrowRight, LogInIcon } from 'lucide-react'
 import FileUpload from "@/components/FileUpload";
+import { checkSubscription } from "@/lib/subscription";
+import SubscriptionButton from "@/components/SubscriptionButton";
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
+import { eq } from 'drizzle-orm'
+import Link from "next/link";
 
 export default async function Home() {
   const { userId } = await auth()
-  const isAuth = !!userId
+  const isAuth = !!userId;
+  const isPro = await checkSubscription();
+  let firstChat;
+  if (userId) {
+    firstChat = await db.select().from(chats).where(eq(chats.userId, userId))
+    if (firstChat) {
+      firstChat = firstChat[0]
+    }
+  }
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -17,7 +30,12 @@ export default async function Home() {
           </div>
 
           <div className="flex mt-2">
-            { isAuth && <Button>Go to Chats</Button>}
+            { isAuth && firstChat && (
+              <Link href={`/chat/${firstChat.id}`}>
+                <Button>Go to Chats < ArrowRight className="ml-2" /></Button>
+              </Link>
+            )}
+            <div className="ml-3"><SubscriptionButton isPro={isPro} /></div>
           </div>
 
           <p className="max-w-xl mt-2 text-lg text-slate-600">
