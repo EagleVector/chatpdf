3220d002e629da55b2422dd7045e8d50b370ac49
EagleVector <kumarsubhambili3@gmail.com>
2023-09-29 11:33:03 +0530

    user subscription schema design

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -14,10 +14,23 @@ export const chats = pgTable('chats', {
 
 export type DrizzleChat = typeof chats.$inferSelect;
 
-export const message = pgTable("messages", {
+export const messages = pgTable("messages", {
   id: serial("id").primaryKey(),
   chatId: integer('chat_id').references(() => chats.id).notNull(),
   content: text('content').notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   role: userSystemEnum('role').notNull()
-})
+});
+
+export const userSubscriptions = pgTable("user_subscriptions", {
+  id: serial("id").primaryKey(),
+  userId: varchar("user_id", { length: 256 }).notNull().unique(),
+  stripeCustomerId: varchar("stripe_customer_id", { length: 256 })
+    .notNull()
+    .unique(),
+  stripeSubscriptionId: varchar("stripe_subscription_id", {
+    length: 256,
+  }).unique(),
+  stripePriceId: varchar("stripe_price_id", { length: 256 }),
+  stripeCurrentPeriodEnd: timestamp("stripe_current_period_ended_at"),
+});
